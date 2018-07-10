871b5b2e00ca0ff48c279fd2c210bcb6f631d209
unknown <clement.cazaubon@epitech.eu>
2018-07-10 16:45:14 +0200

    [arduino] add mix pump functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,15 @@ const int PUMP = 3;
 const int EV = 4;
 const int EVV = 5;
 const int PUMPP = 6;
+const int MELPUMP = 7;
 
 void startPump(int pumpid, int evid)
 {
   digitalWrite(pumpid, HIGH);
   digitalWrite(evid, HIGH);
 }
+void startMelPump(int pumpid) {digitalWrite(pumpid, HIGH);}
+void stopMelPump(int pumpid) {digitalWrite(pumpid, LOW);}
 
 void stopPump(int pumpid, int evid)
 {
@@ -26,20 +29,24 @@ void setup() {
   pinMode(EV, OUTPUT);
   pinMode(PUMPP, OUTPUT);
   pinMode(EVV, OUTPUT);
+  pinMode(MELPUMP, OUTPUT);
   digitalWrite(PU, LOW);
   digitalWrite(PUMP, LOW);
   digitalWrite(EV, LOW);
   digitalWrite(PUMPP, LOW);
   digitalWrite(EVV, LOW);
+  digitalWrite(MELPUMP, LOW);
 }
 
 void loop() {
   startPump(PUMP, EV);
   startPump(PUMPP, EVV);
+  startMelPump(MELPUMP);
   startPulv(PU);
   delay(10000);
   stopPump(PUMP, EV);
   stopPump(PUMPP, EVV);
+  stopMelPump(MELPUMP);
   stopPulv(PU);
   delay(3000);
 }
